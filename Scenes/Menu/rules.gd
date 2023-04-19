c9143dab9b778c2b90f5efc8276e394c8c6f84e8
Gabriel Sales <gviana2@slb.com>
2023-04-19 20:31:39 -0300

    Add room scene with capy moving

--- Scenes/Menu/rules.gd
+++ Scenes/Menu/rules.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/puzzle_one.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Room.tscn")
---
+++ Scenes/Room.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var capy_position = 'middle'
+var moving = false
+
+func move(new_position):
+	if moving:
+		return
+	
+	moving = true
+	
+	var animation_name = "%s_to_%s" % [capy_position, new_position]
+	capy_position = new_position
+	
+	$Capy/CapyAnimationPlayer.play(animation_name)
+
+func _on_left_table_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton and event.pressed):
+		move("left")
+
+func _on_middle_table_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton and event.pressed):
+		move("middle")
+
+func _on_right_table_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton and event.pressed):
+		move("right")
+
+func _on_capy_animation_player_animation_finished(anim_name):
+	moving = false
