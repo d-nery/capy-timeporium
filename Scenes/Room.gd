e9ca1c30b8d776216843e66e6c6865df3b6f42ab
Gabriel Sales <gviana2@slb.com>
2023-04-19 20:58:59 -0300

    Start puzzle after animation or same position click

--- Scenes/Room.gd
+++ Scenes/Room.gd
@@ -3,26 +3,28 @@ extends Node2D
 var capy_position = 'middle'
 var moving = false
 
-func move(new_position):
-	if moving or new_position == capy_position:
-		return
-	
-	moving = true
-	
-	var animation_name = "%s_to_%s" % [capy_position, new_position]
-	capy_position = new_position
-	
-	$Capy/CapyAnimationPlayer.play(animation_name)
-
-func is_click(event):
-	return event is InputEventMouseButton and event.pressed
+func start_puzzle():
+	get_tree().change_scene_to_file("res://Scenes/puzzle_one.tscn")
 
 func _on_table_input_event(viewport, event, shape_idx, new_position):
 	if event is InputEventMouseButton and event.pressed:
-		move(new_position)
+		if moving:
+			return
+			
+		if capy_position == new_position:
+			start_puzzle()
+			return
+			
+		moving = true
+	
+		var animation_name = "%s_to_%s" % [capy_position, new_position]
+		capy_position = new_position
+		
+		$Capy/CapyAnimationPlayer.play(animation_name)
 
 func _on_capy_animation_player_animation_finished(anim_name):
 	moving = false
+	start_puzzle()
 
 func _on_table_mouse_entered():
 	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
