506bc03ce720dc1aba07d0f976cdec413bd939f1
Giulio Sá <giuliocaetano@yahoo.com.br>
2023-04-21 13:58:52 -0300

    Changes

--- Scenes/Menu/bottom_bar.gd
+++ Scenes/Menu/bottom_bar.gd
@@ -8,6 +8,9 @@ func _process(delta):
 	timer_text.text = "%d:%02d" % [floor(timer.time_left / 60), int(timer.time_left) % 60]
 	coins.text = str(CoinCounter.coins)
 	
+	if (CoinCounter.coins >= 100):
+		get_tree().change_scene_to_file("res://Scenes/credits.tscn")
+	
 
 func start_timer():
 	$Control/HBoxContainer/HBoxContainer2/Timer.start(30)
