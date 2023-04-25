b1cd0c1f322212c2d69651e6fcb936ee11513943
Giulio Sá <giuliocaetano@yahoo.com.br>
2023-04-25 17:28:56 -0300

    Adding new frames and fix credits

--- Scenes/credits.gd
+++ Scenes/credits.gd
@@ -7,3 +7,9 @@ func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		RoomState.restart()
 		get_tree().change_scene_to_file("res://Scenes/Menu/start_screen.tscn")
+
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "credits":
+		get_tree().change_scene_to_file("res://Scenes/Menu/start_screen.tscn")
